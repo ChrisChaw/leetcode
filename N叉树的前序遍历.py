bbe92ef9c176636f77fe54cae219f2b6d74bb8ec
caohaiying <1345647614@qq.com>
2021-03-29 19:58:16 +0800

    6

---
+++ N叉树的前序遍历.py
@@ -0,0 +1,17 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val=None, children=None):
+        self.val = val
+        self.children = children
+"""
+
+
+class Solution:
+    def preorder(self, root: 'Node') -> List[int]:
+        if not root:
+            return []
+        res = list()
+        for leaf in root.children:
+            res += self.preorder(leaf)
+        return [root.val] + res
