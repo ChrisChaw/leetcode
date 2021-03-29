d31b6536798300ebd750e7aa4e9d4beec8a1c012
caohaiying <1345647614@qq.com>
2021-03-30 00:41:07 +0800

    20

---
+++ 移动零.py
@@ -0,0 +1,9 @@
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        """
+        Do not return anything, modify nums in-place instead.
+        """
+        n = nums.count(0)
+        for i in range(n):
+            nums.remove(0)
+        nums.extend([0] * n)
