dc62bd171f85ed67026d9ed2f83f6fa29b057015
caohaiying <1345647614@qq.com>
2021-03-30 13:58:36 +0800

    12

--- 有效的括号.py
+++ 有效的括号.py
@@ -1,18 +1,18 @@
 class Solution:
     def isValid(self, s: str) -> bool:
         stack = []
-        match = {'}': '{', ')': '(', ']': '['}
+        match = {'}':'{', ')':'(', ']':'['}
         for ch in s:
-            if ch in {'(', '[', '{'}:
+            if ch in {'(','[','{'}:
                 stack.append(ch)
             elif len(stack) > 0:
-                if match[ch] == stack[-1]:  # 匹配就出栈
+                if match[ch] == stack[-1]: # 与栈顶元素相匹配 如果能匹配就出栈
                     stack.pop()
-                else:
+                else: # 与栈顶元素相匹配 如果不能匹配 字符串无效
                     return False
-            else:  # len(stack) == 0
+            else: # len(stack) == 0 如果字符串里没有右括号 字符串无效
                 return False
-        if len(stack) == 0:
+        if len(stack) == 0: # 最后 栈为空了 说明字符串有效
             return True
-        else:
+        else: # 最后 栈不为空 说明字符串无效
             return False
