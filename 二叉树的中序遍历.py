2e58aef73f01d591f1246f98d48f5a960a9303eb
caohaiying <1345647614@qq.com>
2021-03-30 21:42:34 +0800

    2

--- 二叉树的中序遍历.py
+++ 二叉树的中序遍历.py
@@ -11,3 +11,26 @@ class Solution:
                 result.append(get_node.val)
                 get_node = get_node.right
         return result
+
+"""
+其核心思想如下：
+使用颜色标记节点的状态，新节点为白色，已访问的节点为灰色。
+如果遇到的节点为白色，则将其标记为灰色，然后将其右子节点、自身、左子节点依次入栈。
+(因为栈是先进后出的 而中序遍历是:左根右 所以入栈顺序是:右根左)
+如果遇到的节点为灰色，则将节点的值输出。
+"""
+class Solution:
+    def inorderTraversal(self, root: TreeNode) -> List[int]:
+        WHITE, GRAY = 0, 1
+        res = []
+        stack = [(WHITE, root)]
+        while stack:
+            color, node = stack.pop()
+            if node is None: continue
+            if color == WHITE:
+                stack.append((WHITE, node.right))
+                stack.append((GRAY, node))
+                stack.append((WHITE, node.left))
+            else:
+                res.append(node.val)
+        return res
