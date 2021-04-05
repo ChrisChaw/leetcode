0b754a55c8545081a1dfd5a344f78474bf7d3263
caohaiying <1345647614@qq.com>
2021-04-05 22:45:05 +0800

    46

---
+++ 合并区间.py
@@ -0,0 +1,16 @@
+def merge(intervals):
+    intervals.sort(key=lambda x: x[0])
+    merged = []
+    for interval in intervals:
+        # 如果列表为空，或者当前区间与上一区间不重合，直接添加
+        if not merged or merged[-1][1] < interval[0]:
+            merged.append(interval)
+        else:  # 理论依据 所有能够合并的区间都必然是连续的
+            # 否则的话，我们就可以与上一区间进行合并
+            merged[-1][1] = max(merged[-1][1], interval[1])
+
+    return merged
+
+
+intervals = [[1, 3], [2, 6], [8, 10], [15, 18]]
+print(merge(intervals))
