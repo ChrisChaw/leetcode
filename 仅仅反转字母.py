b2d08c97bad461d553fd368c1f6ffe0407fac4f3
caohaiying <1345647614@qq.com>
2021-04-07 01:20:36 +0800

    58

---
+++ 仅仅反转字母.py
@@ -0,0 +1,10 @@
+class Solution:
+    def reverseOnlyLetters(self, S: str) -> str:
+        letters = [c for c in S if c.isalpha()]
+        ans = []
+        for c in S:
+            if c.isalpha():
+                ans.append(letters.pop())
+            else:
+                ans.append(c)
+        return "".join(ans)
