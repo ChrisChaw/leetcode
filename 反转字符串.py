0bc902a2d8856d577219512739d171fca0237089
caohaiying <1345647614@qq.com>
2021-04-07 00:05:50 +0800

    53

---
+++ 反转字符串.py
@@ -0,0 +1,16 @@
+class Solution:
+    def reverseString(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        if s == '':
+            return
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            tmp = s[i]
+            s[i] = s[j]
+            s[j] = tmp
+            i += 1
+            j -= 1
+        return s
