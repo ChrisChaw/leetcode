65235a989930b89e631be311d5e3930350863cd1
caohaiying <1345647614@qq.com>
2021-04-08 14:16:59 +0800

    62

---
+++ 最长递增子序列.py
@@ -0,0 +1,18 @@
+class Solution:
+    """
+    时间复杂度：O(N^2)
+    空间复杂度：O(N)
+    """
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        length = len(nums)
+        if length < 2:
+            return length
+        dp = [1 for _ in range(length)]
+        for i in range(1, length):
+            for j in range(0, i):
+                if nums[j] < nums[i]:
+                    dp[i] = max(dp[i], dp[j] + 1)
+        res = 0
+        for i in range(length):
+            res = max(dp[i], res)
+        return res
