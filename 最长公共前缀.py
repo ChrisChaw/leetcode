feed518c4553a091db55744c4dd4c9e3136a856d
caohaiying <1345647614@qq.com>
2021-04-14 03:53:14 +0800

    77

--- 最长公共前缀.py
+++ 最长公共前缀.py
@@ -4,7 +4,7 @@ class Solution:
             return ""
         for i in range(0, len(strs[0])):
             c = strs[0][i]
-            for j in range(1, len(strs)):
-                if i == len(strs[j]) or strs[j][i] != c:
-                    return strs[0][:i]
+            for j in range(1, len(strs)):  # 第j个字符串
+                if i == len(strs[j]) or strs[j][i] != c:  # i:第j个字符串的长度 strs[j][i]:第j个字符串的第i个字符
+                    return strs[0][:i]  # 如果在第i个字符处不相同了
         return strs[0]
