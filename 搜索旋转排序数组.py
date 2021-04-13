2c957da0dfdb520e6582592f7ab928f99a021e92
caohaiying <1345647614@qq.com>
2021-04-14 01:58:00 +0800

    77

--- 搜索旋转排序数组.py
+++ 搜索旋转排序数组.py
@@ -14,6 +14,6 @@ class Solution:  # 二分查找 时间复杂度：O(lgn)
             else:
                 high = mid
         if low == high and nums[low] == target:
-            return nums.index(target)
+            return low
         else:
             return -1
