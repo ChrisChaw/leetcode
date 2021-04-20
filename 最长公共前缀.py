5cf148a267d0a5e3246dfb28bf2cd68e32180e19
caohaiying <1345647614@qq.com>
2021-04-20 13:29:17 +0800

    78

--- 最长公共前缀.py
+++ 最长公共前缀.py
@@ -3,7 +3,7 @@ class Solution:
         if strs == '' or len(strs) == 0:
             return ""
         for i in range(0, len(strs[0])):
-            c = strs[0][i]
+            c = strs[0][i]  # strs[0][i]:第0个字符串的第i个字符 为c
             for j in range(1, len(strs)):  # 第j个字符串
                 if i == len(strs[j]) or strs[j][i] != c:  # i:第j个字符串的长度 strs[j][i]:第j个字符串的第i个字符
                     return strs[0][:i]  # 如果在第i个字符处不相同了
