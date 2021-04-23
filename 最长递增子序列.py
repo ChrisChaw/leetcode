efb9681edf68499c62c5cc6d3b4e6dda506521ff
caohaiying <1345647614@qq.com>
2021-04-24 00:07:56 +0800

    79

--- 最长递增子序列.py
+++ 最长递增子序列.py
@@ -17,3 +17,33 @@ class Solution:
         for i in range(length):
             res = max(dp[i], res)
         return res
+
+
+class Solution:
+    """
+    时间复杂度：O(NlogN)
+    空间复杂度：O(N) 最长递增子序列 可以不连续
+    """
+
+    def lengthOfLIS(self, nums: List[int]) -> int:
+        if not nums:
+            return 0
+        # dp + 二分查找 时间复杂度: O(NlogN)
+        tail = [nums[0]]  # 保存其中一个最长上升子序列
+        res = 1  # 最终返回的长度
+        for num in nums[1:]:
+            if num > tail[-1]:
+                tail.append(num)
+                res += 1
+            else:
+                # 二分查找tail中第一个比num大的数 并替换为num
+                i, j = 0, res
+                while i < j:
+                    mid = i + (j - i) // 2  # 防止i,j过大 计算溢出
+                    if tail[mid] > num:
+                        i = mid + 1
+                    else:
+                        j = mid
+                tail[i] = num  # i和j交汇了
+        print(tail)
+        return res
