fd1f65219d7dbaa7c4be6ab0a2fcb60727a8804f
caohaiying <1345647614@qq.com>
2021-04-28 19:22:43 +0800

    88

--- 括号匹配.py
+++ 括号匹配.py
@@ -2,10 +2,10 @@ def check_brack(exp):
     stack = []
     match = {'}': '{', ']': '[', ')': '('}
     for ch in exp:
-        if ch in {'(', '[', '{'}:  # 遇到右括号 就进栈
+        if ch in {'(', '[', '{'}:  # 遇到右括号 就进栈(栈里都是右括号)
             stack.append(ch)
         elif len(stack) > 0:
-            if match[ch] == stack[-1]:  # 遇到左括号 就出栈
+            if match[ch] == stack[-1]:  # 遇到左括号 就出栈顶的右括号出栈
                 stack.pop()
             else:
                 return False
