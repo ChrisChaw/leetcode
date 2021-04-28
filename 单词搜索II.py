75a53fa3c5191354db7234eeb40595ba8c61b6d3
caohaiying <1345647614@qq.com>
2021-04-28 17:57:36 +0800

    88

--- 单词搜索II.py
+++ 单词搜索II.py
@@ -10,7 +10,7 @@ class Solution:
         if not words:
             return []
         self.result = set()
-
+        import collections
         # 构建trie 并把单词insert到trie里
         root = collections.defaultdict()
         for word in words:
