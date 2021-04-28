e350062eebb7a88f6ee33e458a159e288d7dfab8
caohaiying <1345647614@qq.com>
2021-04-28 19:47:20 +0800

    88

--- 搜索旋转排序数组.py
+++ 搜索旋转排序数组.py
@@ -8,7 +8,7 @@ class Solution:  # 二分查找 时间复杂度：O(lgn)
             if (nums[0] <= nums[mid] and (nums[mid] < target or nums[0] > target)):
                 low = mid + 1
             # 当nums[0]>nums[mid]时 说明[0, mid]发生了旋转，向后规约条件 eg:nums=[5,6,2,3,4,7,9] target=4
-            elif nums[mid] < target < nums[0]:
+            elif nums[0] > target > nums[mid]:
                 low = mid + 1
             # 当向前规约时
             else:
