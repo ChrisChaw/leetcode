9ca6401ffa64469a3dfcac554f5155c438bfce0a
caohaiying <1345647614@qq.com>
2021-04-28 22:07:08 +0800

    88

--- 柱状图中的最大矩形.py
+++ 柱状图中的最大矩形.py
@@ -10,7 +10,7 @@ class Solution:
                 stack.pop()  # 弹出栈顶元素 下一个元素变成栈顶元素了
             # 如果栈不为空:栈顶元素为当前元素的索引i的左边界; 如果栈为空:左边界为-1
             left[i] = stack[-1] if stack else -1
-            stack.append(i)  # 当前元素i入栈
+            stack.append(i)  # 当前元素的索引i入栈
         # 面积 = 当前元素高度 * (当前元素右边界-当前元素左边界-offset)
         ans = max((right[i] - left[i] - 1) * heights[i] for i in range(n)) if n > 0 else 0
         return ans
