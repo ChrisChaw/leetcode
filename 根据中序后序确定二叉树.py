0de529f089c6ca20dac0b161258b9119751ebb5f
caohaiying <1345647614@qq.com>
2022-05-26 12:55:08 +0800

    inorder_postorder

--- 根据中序后序确定二叉树.py
+++ 根据中序后序确定二叉树.py
@@ -4,7 +4,7 @@ class Node:
         self.left = left
         self.right = right
 
-    def build_tree_from_inorder_and_postorder(inorder: List[int], postorder: List[int]) -> Node:
+    def build_tree_from_inorder_and_postorder(self, inorder: List[int], postorder: List[int]) -> Node:
         if not (postorder and inorder):
             return None
 
@@ -14,4 +14,4 @@ class Node:
 
         root.left = self.build_tree_from_inorder_and_postorder(inorder[:mid_inx], postorder[:mid_inx])
 
-        root.right = build_tree_from_inorder_and_postorder(inorder[mid_inx + 1:], postorder[mid_inx:-1])
+        root.right = self.build_tree_from_inorder_and_postorder(inorder[mid_inx + 1:], postorder[mid_inx:-1])
